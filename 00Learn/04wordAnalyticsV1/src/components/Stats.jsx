3329e424207986687560fb67563b56c529eef012
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-25 14:40:27 +0530

    04WordAnalytics->Stat DRY component added inside STats

--- 00Learn/04wordAnalyticsV1/src/components/Stats.jsx
+++ 00Learn/04wordAnalyticsV1/src/components/Stats.jsx
@@ -2,7 +2,20 @@
 export default function Stats() {
    return (
       <section className='stats'>
-         s
+         <Stat number={0} title={'Words'} />
+         <Stat number={'0'} title={'Characters'} />
+         <Stat number={'280'} title={'Instagram'} />
+         <Stat number={'2200'} title={'Facebook'} />
+      </section>
+   )
+}
+
+function Stat({ number, title }) {
+   return (
+
+      <section className="stat">
+         <span className="stat__number">{number}</span>
+         <h2 className="second-heading">{title}</h2>
       </section>
    )
 }
