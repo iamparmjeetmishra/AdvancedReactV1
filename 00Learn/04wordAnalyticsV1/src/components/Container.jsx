045c331d0b6a20e83f002a61a658beab4a2f3285
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-25 15:29:11 +0530

    04WordAnalytics->Updated stat with seperate constants file

--- 00Learn/04wordAnalyticsV1/src/components/Container.jsx
+++ 00Learn/04wordAnalyticsV1/src/components/Container.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
+import { FB_MAX_CHARS, INSTA_MAX_CHARS } from "../lib/constants";
 
 export default function Container() {
   const [text, setText] = useState('')
@@ -8,8 +9,8 @@ export default function Container() {
   const stat = {
     words: text.split(/\s/).filter((word) => word !== '').length,
     characters: text.length,
-    instaWord: 280 - text.length,
-    fbWord: 2200 - text.length
+    instaWord: INSTA_MAX_CHARS - text.length,
+    fbWord: FB_MAX_CHARS - text.length
   }
 
   return (
