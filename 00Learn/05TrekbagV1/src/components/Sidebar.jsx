a2626baa81f137e6f07dc6ba43054b87bb650c84
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-26 14:16:54 +0530

    05TrekbagV1->Sidebar Component

---
+++ 00Learn/05TrekbagV1/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import AddItemForm from "./AddItemForm";
+import ButtonGroup from "./ButtonGroup";
+
+export default function Sidebar({setItems}) {
+  
+  return (
+    <div className='sidebar'>
+      <AddItemForm setItems={setItems} />
+      <ButtonGroup />
+    </div>
+  )
+}
