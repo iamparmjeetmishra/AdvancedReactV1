9159956366016962fb79094a69ef5231a36443eb
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-26 14:19:48 +0530

    06TrekBagV2->Button now accept props like onclick and name

---
+++ 00Learn/06TrekbagV2/src/components/Button.jsx
@@ -0,0 +1,6 @@
+
+export default function Button({buttonType, children, onClick}) {
+  return (
+    <button onClick={onClick} className={`btn ${buttonType === 'secondary' ? 'btn--secondary' : ''}`}>{ children }</button>
+  )
+}
