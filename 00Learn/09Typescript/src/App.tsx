bcb8cdc932086d35bd2306dea639e9d86e20e9a8
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-27 14:45:26 +0530

    09TS->Project Init including shadcn and tailwind

---
+++ 00Learn/09Typescript/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">H2</h2>
+      <Button>Btn</Button>
+    </div>
+  )
+}
