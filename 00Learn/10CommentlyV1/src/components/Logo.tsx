95ba0f0e76d2726b7a952c08e0bae6086335e7ab
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-28 15:02:30 +0530

    10CommentlyV1->Logo a tag is fixed

--- 00Learn/10CommentlyV1/src/components/Logo.tsx
+++ 00Learn/10CommentlyV1/src/components/Logo.tsx
@@ -1,6 +1,7 @@
-
 export default function Logo() {
-  return <a href="/" className="logo">
-    <img src="/commently.png" alt="logo" /> Commently
-  </a>
+	return (
+		<a href="/" className="logo">
+			<img src="/commently.png" alt="logo" /> <span className="logoText">Commently</span>
+		</a>
+	);
 }
