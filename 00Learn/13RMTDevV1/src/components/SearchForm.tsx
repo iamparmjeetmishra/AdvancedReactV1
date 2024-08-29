1ea73db380368cbcb004d6726fb86940d3200892
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-29 23:42:19 +0530

    13RMTDevV1->App design is added and search function

--- 00Learn/13RMTDevV1/src/components/SearchForm.tsx
+++ 00Learn/13RMTDevV1/src/components/SearchForm.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState('')
+
+  const handleSearchForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
+  }
+  console.log(searchText)
+
   return (
-    <form action="#" className="search">
+    <form onSubmit={handleSearchForm} className="search">
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={handleSearchInput}
         spellCheck="false"
         type="text"
         required
