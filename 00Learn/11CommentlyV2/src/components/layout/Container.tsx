234cd90610d8aa3b96e8a3e6c7f8c6278aa1ebd8
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-29 19:23:54 +0530

    11CommentlyV2->container updated

--- 00Learn/11CommentlyV2/src/components/layout/Container.tsx
+++ 00Learn/11CommentlyV2/src/components/layout/Container.tsx
@@ -1,19 +1,11 @@
-import { TFeedbackItem } from "../../lib/types";
 import FeedbackList from "../feedback/FeedbackList";
 import Header from "./Header";
 
-type TContainerProp = {
-  handleAddToList: (text: string) => void;
-  loading: boolean;
-  errMessage: string;
-  feedbackItems: TFeedbackItem[];
-}
-
-export default function Container({handleAddToList, loading, errMessage, feedbackItems}:TContainerProp) {
-  return (
-    <main className="container">
-      <Header handleAddToList={handleAddToList} />
-      <FeedbackList loading={loading} errMessage={errMessage} feedbackItems={feedbackItems} />
-    </main>
-  )
+export default function Container() {
+	return (
+		<main className="container">
+			<Header />
+			<FeedbackList />
+		</main>
+	);
 }
