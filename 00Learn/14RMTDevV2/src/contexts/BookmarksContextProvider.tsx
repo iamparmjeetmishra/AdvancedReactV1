3a6ca8645144d1d4e49a618a069b005427f95206
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-31 18:54:03 +0530

    14RMTDevV2->Feature->BookMarked-Localstorage

--- 00Learn/14RMTDevV2/src/contexts/BookmarksContextProvider.tsx
+++ 00Learn/14RMTDevV2/src/contexts/BookmarksContextProvider.tsx
@@ -1,10 +1,13 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const BookmarksContext = createContext(null);
 
 export default function BookmarksContextProvider({ children }) {
-	const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
-	console.log(bookmarkedIds)
+	
+	const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(() => JSON.parse(
+		localStorage.getItem("bookmarkedIds") || "[]"
+	));
+	console.log(bookmarkedIds);
 	const handleToggleBookmark = (id: number) => {
 		if (bookmarkedIds.includes(id)) {
 			setBookmarkedIds((prev) => prev.filter((item) => item !== id));
@@ -12,6 +15,14 @@ export default function BookmarksContextProvider({ children }) {
 			setBookmarkedIds((prev) => [...prev, id]);
 		}
 	};
+
+	useEffect(() => {
+		localStorage.setItem(
+			"bookmarkedIds",
+			JSON.stringify(bookmarkedIds)
+		);
+	}, [bookmarkedIds]);
+
 	return (
 		<BookmarksContext.Provider
 			value={{
