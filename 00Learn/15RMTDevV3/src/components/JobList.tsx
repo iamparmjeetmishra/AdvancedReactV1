fa003f93adf4ff63ad80f164025f8f87217175b8
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-01 02:00:35 +0530

    15RMTDevV3->JobList now use useJobItemContextProvider

--- 00Learn/15RMTDevV3/src/components/JobList.tsx
+++ 00Learn/15RMTDevV3/src/components/JobList.tsx
@@ -1,20 +1,16 @@
-import { useActiveIdContext } from "../lib/hooks";
-import { TJobItem } from "../lib/type";
+import { useActiveIdContext, useJobItemsContext } from "../lib/hooks";
 import JobListItem from "./JobListItem";
 import Spinner from "./Spinner";
 
-type TJobListItemProps = {
-	jobItems: TJobItem[];
-	isLoading: boolean;
-};
 
-export function JobList({ jobItems, isLoading }: TJobListItemProps) {
-	const {activeId: id} = useActiveIdContext();
+export function JobList() {
+	const { activeId: id } = useActiveIdContext();
+	const { jobItemsSortedAndSliced, isLoading} = useJobItemsContext()
 	return (
 		<ul className="job-list">
 			{isLoading && <Spinner />}
 			{!isLoading &&
-				jobItems.map((jobItem) => (
+				jobItemsSortedAndSliced?.map((jobItem) => (
 					<JobListItem
 						key={jobItem.id}
 						jobItem={jobItem}
