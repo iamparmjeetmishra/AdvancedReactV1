e87520cbe5daa4fd930db1975f0ea5d6ba18b204
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-01 01:58:30 +0530

    15RMTDevV3-NewJobItemsContextProvider for rest of the code

---
+++ 00Learn/15RMTDevV3/src/contexts/JobItemsContextProvider.tsx
@@ -0,0 +1,79 @@
+import { createContext, useState } from "react";
+import { useSearchQuery, useSearchTextContext } from "../lib/hooks";
+import { TJobItem, TPageDirection, TSortBy } from "../lib/type";
+import { RESULTS_PER_PAGE } from "../lib/constants";
+
+type TJobItemsContext = {
+	jobItems: TJobItem[] | undefined ;
+	jobItemsSortedAndSliced: TJobItem[] ;
+	isLoading: boolean;
+	totalNumberOfPages: number ;
+	totalNumberOfResults: number ;
+	currentPage: number;
+	sortBy: TSortBy;
+	handleChangePage: (direction: TPageDirection) => void;
+	handleChangeSortBy: (newSortBy: TSortBy) => void;
+};
+
+export const JobItemsContext = createContext<TJobItemsContext | null>(
+	null
+);
+
+export default function JobItemsContextProvider({
+	children,
+}: {
+	children: React.ReactNode;
+   }) {
+   const { debouncedSearchText } = useSearchTextContext()
+	const [currentPage, setCurrentPage] = useState(1);
+	const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
+	const [sortBy, setSortBy] = useState<TSortBy>("relevant");
+
+	// derived
+	const totalNumberOfResults = jobItems?.length || 0;
+	const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
+	const jobItemsSorted =
+		[...(jobItems || [])].sort((a, b) => {
+			if (sortBy === "relevant") {
+				return b.relevanceScore - a.relevanceScore;
+			} else {
+				return a.daysAgo - b.daysAgo;
+			}
+		}) || [];
+	const jobItemsSortedAndSliced = jobItemsSorted?.slice(
+		currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+		currentPage * RESULTS_PER_PAGE
+	);
+
+	// event
+
+	const handleChangePage = (direction: TPageDirection) => {
+		if (direction === "next") {
+			setCurrentPage((prev) => prev + 1);
+		} else if (direction === "previous") {
+			setCurrentPage((prev) => prev - 1);
+		}
+	};
+
+	const handleChangeSortBy = (newSortBy: TSortBy) => {
+		setCurrentPage(1);
+		setSortBy(newSortBy);
+	};
+	return (
+		<JobItemsContext.Provider
+			value={{
+				jobItems,
+				jobItemsSortedAndSliced,
+				isLoading,
+				totalNumberOfPages,
+				totalNumberOfResults,
+				currentPage,
+				sortBy,
+				handleChangePage,
+				handleChangeSortBy,
+			}}
+		>
+			{children}
+		</JobItemsContext.Provider>
+	);
+}
