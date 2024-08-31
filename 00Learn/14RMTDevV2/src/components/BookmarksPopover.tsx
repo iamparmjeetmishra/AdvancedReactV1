7cfd9e921c925309afd5c8bca42b9818d10bb383
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-31 22:59:07 +0530

    14RMTDevV2->we can use underscore if props is not using

--- 00Learn/14RMTDevV2/src/components/BookmarksPopover.tsx
+++ 00Learn/14RMTDevV2/src/components/BookmarksPopover.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 import { useBookmarksContext } from "../lib/hooks";
 import JobList from "./JobList";
 
-const BookmarksPopover = forwardRef<HTMLDivElement>(function (props, ref) {
+const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext()
   
   return <div ref={ref} className="bookmarks-popover">
