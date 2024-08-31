5eb3d9832555fbd8eb9f2bc7d7bf35dce1335226
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-01 01:59:54 +0530

    15RMTDevV3-App-All props and states and events removed

--- 00Learn/15RMTDevV3/src/components/App.tsx
+++ 00Learn/15RMTDevV3/src/components/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -12,82 +11,28 @@ import Pagination from "./PaginationControls";
 import JobList from "./JobList";
 import Sorting from "./SortingControls";
 import ResultsCount from "./ResultsCount";
-import { useDebounce, useSearchQuery } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
-import { RESULTS_PER_PAGE } from "../lib/constants";
-import { TPageDirection, TSortBy } from "../lib/type";
 
 function App() {
-	//state
-	const [searchText, setSearchText] = useState("");
-	const [currentPage, setCurrentPage] = useState(1);
-	const debouncedSearchText = useDebounce(searchText);
-	const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
-	const [sortBy, setSortBy] = useState<TSortBy>("relevant");
-
-	// derived
-	const jobItemsNumber = jobItems?.length || 0;
-	const totalNumOfPage = jobItemsNumber / RESULTS_PER_PAGE;
-	const jobItemsSorted =
-		[...(jobItems || [])].sort((a, b) => {
-			if (sortBy === "relevant") {
-				return b.relevanceScore - a.relevanceScore;
-			} else {
-				return a.daysAgo - b.daysAgo;
-			}
-		}) || [];
-	const jobItemsSortedAndSliced = jobItemsSorted?.slice(
-		currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-		currentPage * RESULTS_PER_PAGE
-	);
-
-	// event
-	const handleChangePage = (direction: TPageDirection) => {
-		if (direction === "next") {
-			setCurrentPage((prev) => prev + 1);
-		} else if (direction === "previous") {
-			setCurrentPage((prev) => prev - 1);
-		}
-	};
-
-	const handleChangeSortBy = (newSortBy: TSortBy) => {
-		setCurrentPage(1);
-		setSortBy(newSortBy);
-	};
-
-	
-
 	return (
 		<>
 			<Background />
 			<Header>
 				<HeaderTop>
 					<Logo />
-					<BookmarksButton  />
+					<BookmarksButton />
 				</HeaderTop>
-				<SearchForm
-					searchText={searchText}
-					setSearchText={setSearchText}
-				/>
+				<SearchForm />
 			</Header>
 
 			<Container>
 				<Sidebar>
 					<SidebarTop>
-						<ResultsCount
-							count={jobItemsNumber > 0 ? jobItemsNumber : 0}
-						/>
-						<Sorting sortBy={sortBy} onClick={handleChangeSortBy} />
+						<ResultsCount />
+						<Sorting />
 					</SidebarTop>
-					<JobList
-						jobItems={jobItemsSortedAndSliced}
-						isLoading={isLoading}
-					/>
-					<Pagination
-						onClick={handleChangePage}
-						currentPage={currentPage}
-						totalNumOfPage={totalNumOfPage}
-					/>
+					<JobList />
+					<Pagination	/>
 				</Sidebar>
 				<JobItemContent />
 			</Container>
