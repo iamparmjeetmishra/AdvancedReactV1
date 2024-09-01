e4c07b40f1e71793965a787b3ccec26b20637527
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-01 22:45:42 +0530

    16EventoV1->TwMerge added for class conflict

--- 00Learn/16eventov1/src/app/events/[city]/page.tsx
+++ 00Learn/16eventov1/src/app/events/[city]/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-	return <div>Page</div>;
+import EventList from "@/components/event-list";
+import H1 from "@/components/h1";
+import { TEventoEvent } from "@/lib/types";
+import { TextCapitalize } from "@/lib/utils";
+import toast from "react-hot-toast";
+
+type TEventPageProps = {
+	params: {
+		city: string;
+	};
+};
+
+export default async function EventPage({ params }: TEventPageProps) {
+	
+	let res = await fetch(
+		"https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+	);
+	const events: TEventoEvent[] = await res.json();
+	const city = params.city;
+
+	return (
+		<main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+			<H1 className="mb-28">
+				{city === "all" && "All Events"}
+				{city !== "all" && `Events in ${TextCapitalize(city)}`}
+			</H1>
+			<EventList events={events} />
+		</main>
+	);
 }
