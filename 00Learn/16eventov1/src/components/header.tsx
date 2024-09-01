8710604c19ee5af3d16e819c9cd1510e122df22e
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-01 20:17:01 +0530

    16EventoV1->Header updated for active link and motion added

--- 00Learn/16eventov1/src/components/header.tsx
+++ 00Learn/16eventov1/src/components/header.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
 import Logo from "./logo";
-
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from 'framer-motion'
 const routes = [
 	{
 		name: "Home",
@@ -13,14 +16,27 @@ const routes = [
 ];
 
 export default function Header() {
+	const activePathname = usePathname();
 	return (
 		<header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
 			<Logo />
-			<nav>
-				<ul className="flex gap-x-6 text-sm">
+			<nav className="h-full">
+				<ul className="flex gap-x-6 h-full text-sm">
 					{routes.map((route) => (
-						<li key={route.path} className="text-white/50 hover:text-white transition">
+						<li
+							key={route.path}
+							className={clsx(
+								"flex items-center hover:text-white relative transition",
+								{
+									"text-white": route.path === activePathname,
+									"text-white/50": route.path !== activePathname,
+								}
+							)}
+						>
 							<Link href={route.path}>{route.name}</Link>
+							{activePathname === route.path && (
+								<motion.div layoutId="header-active-link" className="bg-accent h-1 w-full absolute bottom-0"></motion.div>
+							)}
 						</li>
 					))}
 				</ul>
