4e0dc1b58e995435bf4f9b6c3730f2229dd3e5da
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-02 17:10:37 +0530

    16EventoV1->extracted getting data to utils

--- 00Learn/16eventov1/src/lib/utils.ts
+++ 00Learn/16eventov1/src/lib/utils.ts
@@ -1,3 +1,28 @@
+import clsx, { ClassValue } from 'clsx'
+import { twMerge} from 'tailwind-merge'
+import { TEventoEvent } from './types'
 export const TextCapitalize = (text: string) => {
    return text.charAt(0).toUpperCase() + text.slice(1)
+}
+
+export function cn(...classes: ClassValue[]) {
+   return twMerge(clsx(classes))
+}
+
+export async function sleep(ms:number) {
+   return new Promise((resolve) => {
+      setTimeout(resolve, ms)
+   })
+}
+
+export async function getEvents(city: string) {
+   const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`)
+   const events: TEventoEvent[] = await response.json()
+   return events
+}
+
+export async function getEvent(slug: string) {
+   const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`)
+   const event = await response.json()
+   return event;
 }
