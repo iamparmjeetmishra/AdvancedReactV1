ba26384a3e8951235e09a4eac0e012853cd28c9d
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-04 13:08:00 +0530

    17EventoV2->server utils seperated from utils

--- 00Learn/17eventov2/src/lib/utils.ts
+++ 00Learn/17eventov2/src/lib/utils.ts
@@ -1,8 +1,5 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import prisma from "./db";
-import { notFound } from "next/navigation";
-import { FIX_EVENT_ITEMS } from "./constants";
 
 export const TextCapitalize = (text: string): string => {
 	if (!text) return text;
@@ -17,59 +14,4 @@ export async function sleep(ms: number) {
 	return new Promise((resolve) => {
 		setTimeout(resolve, ms);
 	});
-}
-
-// Event data
-
-export async function getEvents(city: string, page = 1) {
-	try {
-		const filterCity =
-			city === "all" ? undefined : TextCapitalize(city);
-		const events = await prisma.eventoEvent.findMany({
-			where: {
-				city: filterCity,
-			},
-			take: FIX_EVENT_ITEMS,
-			skip: (page - 1) * FIX_EVENT_ITEMS,
-			orderBy: {
-				date: "asc",
-			},
-		});
-
-		let totalCount;
-		if (city === "all") {
-			totalCount = await prisma.eventoEvent.count();
-		} else {
-			totalCount = await prisma.eventoEvent.count({
-				where: {
-					city: TextCapitalize(city),
-				},
-			});
-		}
-
-		if (!events) {
-			return notFound();
-		}
-		return { events, totalCount };
-	} catch (error) {
-		console.log("Error Fetching events:", error);
-		return notFound();
-	}
-}
-
-export async function getEvent(slug: string) {
-	try {
-		const event = await prisma.eventoEvent.findUnique({
-			where: {
-				slug: slug,
-			},
-		});
-		if (!event) {
-			return notFound();
-		}
-		return event;
-	} catch (error) {
-		console.log("Error fetching event", error);
-		return notFound();
-	}
 }
