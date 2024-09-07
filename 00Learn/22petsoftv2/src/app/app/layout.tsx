0c1ca79fc303f78e13fd492a3ae04c9202b1bc1f
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-08 01:32:56 +0530

    22PetSoftV2=> V2 Init

---
+++ 00Learn/22petsoftv2/src/app/app/layout.tsx
@@ -0,0 +1,35 @@
+import AppFooter from "@/components/app-footer";
+import AppHeader from "@/components/app-header";
+import BackgroundPattern from "@/components/background-pattern";
+import PetContextProvider from "@/contexts/pet-context-provider";
+import SearchContextProvider from "@/contexts/search-context-provider";
+import { PetProp } from "@/lib/types";
+
+type LayoutProps = {
+	children: React.ReactNode;
+};
+
+export default async function Layout({ children }: LayoutProps) {
+	const res = await fetch(
+		"https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+	);
+	if (!res.ok) {
+		throw new Error("Could not fetch pets data");
+	}
+	const data: PetProp[] = await res.json();
+
+	return (
+		<>
+			<BackgroundPattern />
+			<div className="flex flex-col min-h-screen max-w-[1050px] mx-auto px-4">
+				<AppHeader />
+				<SearchContextProvider>
+					<PetContextProvider data={data}>
+						{children}
+					</PetContextProvider>
+				</SearchContextProvider>
+				<AppFooter />
+			</div>
+		</>
+	);
+}
