458940a536a34e484f3b3d37f4a3b43e93bdfb9b
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-09 00:11:10 +0530

    22PetSoftV2=> types update with prisma data type

--- 00Learn/22petsoftv2/src/lib/types.ts
+++ 00Learn/22petsoftv2/src/lib/types.ts
@@ -1,8 +1,7 @@
-export type PetProp = {
-	id: string;
-	name: string;
-	ownerName: string;
-	imageUrl: string;
-	age: number;
-	notes: string;
-};
+import { Pet } from "@prisma/client";
+
+export type TPet = Pet
+
+export type TPetEssentials = Omit<Pet, "id" | "createdAt" | "updatedAt">;
+
+export type TPetBtnAction = "add" | "edit";
