a964bbe80b24fd010892656252da2eceda07f031
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-10 00:58:50 +0530

    24PetSoftV4=> Logged in user details added

--- 00Learn/24petsoftv4/src/app/app/account/page.tsx
+++ 00Learn/24petsoftv4/src/app/app/account/page.tsx
@@ -1,14 +1,21 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation";
 
-export default function AccountPage() {
+export default async function AccountPage() {
+	const session = await auth()
+	if (!session?.user) {
+		redirect("/login")
+	}
+	// console.log(session)
 	return (
 		<main className="flex flex-col">
 			
 				<H1 className="text-white py-8">Your Account</H1>
 			
 			<ContentBlock className="h-[500px] flex justify-center items-center">
-				<p>Logged in as ...</p>
+				<p>Logged in as <span className="font-medium">{ session?.user?.email }</span></p>
 			</ContentBlock>
 		</main>
 	);
