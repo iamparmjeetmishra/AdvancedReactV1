222765fecb207eb2d0d2625af3bc40c3b9cbbcb0
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-10 19:43:26 +0530

    25PetSoftV5=> payment page added

---
+++ 00Learn/25petsoftv5/src/app/(auth)/payment/page.tsx
@@ -0,0 +1,10 @@
+import H1 from "@/components/h1";
+import { Button } from "@/components/ui/button";
+
+export default function PaymentPage() {
+   return <main className="flex flex-col gap-3 items-center">
+      <H1>PetSoft access requires payment</H1>
+      <p></p>
+      <Button>Buy lifetime access for $299</Button>
+   </main>
+}
